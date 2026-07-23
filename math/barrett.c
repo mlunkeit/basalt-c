5b2088cb465ae176a9181f0c4724f5586d5bdfae
Malte Lunkeit <malte.lunkeit@gmail.com>
2026-07-23 22:13:01 +0200

    removed unneccessary memset calls

--- math/barrett.c
+++ math/barrett.c
@@ -17,11 +17,9 @@ void barrett_reduce(
     const size_t k = len_modulus;
 
     uint32_t q1[k + 1];
-    memset(q1, 0, (k + 1) * sizeof(uint32_t));
     memcpy(q1, input + k - 1, (k + 1) * sizeof(uint32_t));
 
     uint32_t q2[2 * k + 2];
-    memset(q2, 0, (2 * k + 2) * sizeof(uint32_t));
     bigint_mul_raw(q2, q1, k + 1, mu, k + 1);
 
     uint32_t q3[k + 1];
@@ -31,12 +29,10 @@ void barrett_reduce(
     memcpy(r1, input, (k + 1) * sizeof(uint32_t));
 
     uint32_t r2[2 * k + 1];
-    memset(r2, 0, (2 * k + 1) * sizeof(uint32_t));
     bigint_mul_raw(r2, q3, k + 1, modulus, k);
     memset(r2 + k + 1, 0, sizeof(uint32_t) * k);
 
     uint32_t r[k + 2];
-    memset(r, 0, (k + 2) * sizeof(uint32_t));
     bigint_sub_raw(r, r1, k + 1, r2, k + 1);
 
     result_t rmcmp = bigint_cmp_raw(r, k + 1, modulus, k);
