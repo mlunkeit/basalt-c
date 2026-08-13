c48e76bd8bb4d78487aae8e1d1beb904002c4f1e
Malte Lunkeit <malte.lunkeit@gmail.com>
2026-08-13 16:02:49 +0200

    added implementation to basalt_hmac_sha512 header function

--- src/basalt_hmac.c
+++ src/basalt_hmac.c
@@ -18,3 +18,17 @@ basalt_err_t basalt_hmac_sha256(
 
     return BASALT_OK;
 }
+
+basalt_err_t basalt_hmac_sha512(
+    uint8_t output[BASALT_HS512_DIGEST_SIZE],
+    const uint8_t *key, const size_t len_key,
+    const uint8_t *message, const size_t len_message)
+{
+    if (!output || !key || !message) {
+        return BASALT_ERR_NULL_POINTER;
+    }
+
+    hmac_sha512(output, key, len_key, message, len_message);
+
+    return BASALT_OK;
+}
